94f17cf31fd1a44b226a2d6c42ab73125f5b7486
이정원 <wjddnjswjd12@gmail.com>
2020-11-08 22:49:48 +0900

    proxy설정잘못됐나봄 뭔가가 안됨...

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
 const styles = theme => ({
   root: {
@@ -18,40 +19,29 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
 
-const customers = [
-  {
 
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '권지용',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '박서준',
-    'birthday': '961023',
-    'gender': '남자',
-    'job': '가수'
-  },
-  {
-
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '왕정든',
-    'birthday': '961112',
-    'gender': '남자',
-    'job': '배우'
-  }
-]
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await axios.get('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -69,10 +59,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
-              return (
-                <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
-            })}
+            {this.state.customers ? this.state.customers.map(c => {
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+            }) : ""}
           </TableBody>
         </Table>
 
--- server.js
+++ server.js
@@ -6,8 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
-});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '권지용',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': '대학생'
+        },
+        {
 
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '박서준',
+            'birthday': '961023',
+            'gender': '남자',
+            'job': '가수'
+        },
+        {
+
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '왕정든',
+            'birthday': '961112',
+            'gender': '남자',
+            'job': '배우'
+        }
+
+    ]);
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
